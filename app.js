46b52b5d760f0d33c4f04808e52bc9bcc66ca41a
Igor Datsenko <mr4ort@gmail.com>
2016-08-06 13:44:18 +0300

    add functions to edit table

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
   var btnClose = document.querySelector('[data-remove-row]');
   var tds = table.querySelectorAll('td');
   var activeTD;
+  var input;
 
   function removeRows() {
     var inputs = table.querySelectorAll('input[type="checkbox"]');
@@ -19,28 +20,38 @@
     }
   }
 
-  function changeDd(e) {
-    var activeTD = e.target;
-    var elTag = target.tagName;
+  function changeTd(e) {
+    var elTag = e.target.tagName;
     var val;
 
     if (elTag === "TD") {
+      activeTD = e.target;
       val = activeTD.innerHTML;
       activeTD.innerHTML = '';
-      var input = document.createElement('input');
-      input.setAttribute('type', 'text');
-      input.setAttribute('value', val);
-      activeTD.appendChild(input);
+      // create new element input
+      var el = document.createElement('input');
+      el.setAttribute('type', 'text');
+      el.setAttribute('value', val);
+      // add input element to TD
+      activeTD.appendChild(el);
+      input = activeTD.querySelector('input');
+      input.focus();
+      // add event Listener to save changes
+      input.addEventListener('dblclick', saveTd);
+      input.addEventListener('blur', saveTd);
     }
-
-
   }
+  
   function saveTd() {
-
+    input.removeEventListener('dblclick', saveTd);
+    input.removeEventListener('blur', saveTd);
+    var val = activeTD.querySelector('input').value;
+    activeTD.querySelector('input').remove();
+    activeTD.innerHTML = val;
   }
 
   btnClose.addEventListener('click', removeRows);
-  table.addEventListener('dblclick', changeDd);
+  table.addEventListener('dblclick', changeTd);
 })();
 
 
